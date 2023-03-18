d82fa500d3b75e1c24dc226bca3c37098ff28b7b
amigin <amigin@gmail.com>
2023-03-18 14:39:47 +0200

    Debugging

--- src/attribute_params/attr_parse_utils.rs
+++ src/attribute_params/attr_parse_utils.rs
@@ -3,7 +3,7 @@ pub fn find_params(src: &str) -> (String, Option<String>) {
     if from.is_none() {
         let name = src[1..src.len() - 1].trim().to_string();
 
-        println!("Name: {}", name);
+        println!("Name: [{}]", name);
         return (name, None);
     }
 
