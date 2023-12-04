453faa670271d09f006d3995e23234d9be6c9031
amigin <amigin@gmail.com>
2023-12-04 15:43:27 +0200

    Small Fixes

--- tests/src/test_from_real_life/http_action_result.rs
+++ tests/src/test_from_real_life/http_action_result.rs
@@ -1,26 +1,16 @@
-use types_reader::AnyValueAsStr;
 use types_reader_core as types_reader;
+use types_reader_macros::MacrosParameters;
 
-//#[derive(MacrosParameters)]
-pub struct HttpActionResult<'s> {
-    pub status_code: u16,
-    pub description: &'s str,
-    //#[allow_ident]
-    pub model: Option<&'s str>,
+#[derive(MacrosParameters)]
+pub struct SubParameters<'s> {
+    pub sub_parameter1: &'s str,
+    pub sub_parameter2: &'s str,
 }
 
-impl<'s> TryInto<HttpActionResult<'s>> for &'s types_reader::TokensObject {
-    type Error = syn::Error;
-    fn try_into(self) -> Result<HttpActionResult<'s>, Self::Error> {
-        let result = HttpActionResult {
-            status_code: self.get_named_param("status_code")?.try_into()?,
-            description: self.get_named_param("description")?.try_into()?,
-            model: if let Some(value) = self.try_get_named_param("model") {
-                Some(value.get_value()?.any_value_as_str().as_str())
-            } else {
-                None
-            },
-        };
-        Ok(result)
-    }
+#[derive(MacrosParameters)]
+pub struct HttpActionResult<'s> {
+    pub status_code: u16,
+    pub description: SubParameters<'s>,
+    #[allow_ident]
+    pub model: Option<&'s str>,
 }
